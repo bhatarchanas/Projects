7baff8ee8cff350173a05d3cda4a735d6ff7fe46
bhatarchanas <bhatarchanas@yahoo.com>
2015-09-10 17:06:28 -0400

    Convert a fastq to fasta

---
+++ mock_community/convert_fastq_to_fasta.rb
@@ -0,0 +1,9 @@
+require 'bio'
+
+file = Bio::FlatFile.auto("trimmed/BEI_Mock_Oriented_trimmed.fastq")
+out_file = File.open("trimmed/BEI_Mock_Oriented_trimmed.fasta", "w")
+
+file.each do |entry|
+	out_file.puts(">"+entry.entry_id)
+	out_file.puts(entry.sequence_string)
+end
